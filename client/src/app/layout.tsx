2c07bd00237b822c5c1b6e1591fbd1642e196005
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2025-05-31 22:05:14 +0530

    tyy

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -25,20 +25,20 @@ export const metadata: Metadata = {
   publisher: "Pickleball Association Nagaland",
   icons: {
     icon: [{
-      url: "/favicon-16x16.png",
+      url: "/NPA-logo.png",
       sizes: "16x16",
       type: "image/png"
     }, {
-      url: "/favicon-32x32.png",
+      url: "/NPA-logo.png",
       sizes: "32x32",
       type: "image/png"
     }, {
-      url: "/favicon.ico",
+      url: "/NPA-logo.png",
       sizes: "48x48",
       type: "image/x-icon"
     }],
     apple: [{
-      url: "/apple-touch-icon.png",
+      url: "/NPA-logo.png",
       sizes: "180x180",
       type: "image/png"
     }]
